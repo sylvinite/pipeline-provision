e4e71b71b76166b4dc0f18cd88a07bcb40b4423e
Isak Sylvin <isak.sylvin@scilifelab.se>
2016-05-18 11:49:23 +0200

    Changed ngi reports to use uppmax's pandoc

--- roles/ngi_reports/files/make_report.sh
+++ roles/ngi_reports/files/make_report.sh
@@ -5,8 +5,8 @@ function make_report {
       fn=${f%.md}
       echo "Converting ${f}.."
       PD_DIR=${ngi_reports_dir}/data/pandoc_templates
-      ${PD_DIR}/pandoc --standalone --section-divs ${fn}.md -o ${fn}.html --template=${PD_DIR}/html_pandoc.html --default-image-extension=png --filter ${PD_DIR}/pandoc_filters.py -V template_dir=${PD_DIR}/ 
-      ${PD_DIR}/pandoc --standalone ${fn}.md -o ${fn}.pdf --template=${PD_DIR}/latex_pandoc.tex --latex-engine=xelatex --default-image-extension=pdf --filter ${PD_DIR}/pandoc_filters.py -V template_dir=${PD_DIR}/ 
+      pandoc --standalone --section-divs ${fn}.md -o ${fn}.html --template=${PD_DIR}/html_pandoc.html --default-image-extension=png --filter ${PD_DIR}/pandoc_filters.py -V template_dir=${PD_DIR}/ 
+      pandoc --standalone ${fn}.md -o ${fn}.pdf --template=${PD_DIR}/latex_pandoc.tex --latex-engine=xelatex --default-image-extension=pdf --filter ${PD_DIR}/pandoc_filters.py -V template_dir=${PD_DIR}/ 
     done
 }
 export -f make_report
